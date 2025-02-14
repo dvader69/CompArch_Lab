3a91ee9ff45710c1a7816a53b19a95f1f107e147
dvader69 <145447597+dvader69@users.noreply.github.com>
2025-02-14 23:20:55 +0530

    Update lab3_problem2.v

--- lab3/lab3_problem2.v
+++ lab3/lab3_problem2.v
@@ -105,32 +105,56 @@ reg in_seq,clk,reset;
 wire [2:0] state_reg;
 wire detector_out;
 
-state_machine uut ( .input_reg(in_seq), .clk(clk) , .reset(reset),.output_reg(detector_out),.state_reg(state_reg));
+state_machine uut ( .input_reg(in_seq), .clk(clk) , .reset(reset),.state_reg(state_reg),.output_reg(detector_out));
 initial begin 
     clk=0;
 end
 
 always #5 clk=~clk;
-initial
-    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+//initial
+ //   $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
 initial begin 
     reset=1;
-    #10 reset=0; in_seq=0;
+    #5 reset=0; in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
     #10 in_seq=1;
-
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+    #10 in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+    #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+    #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+    #10 in_seq=0;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+    #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
+    #10 in_seq=1;
+    $monitor(,$time, "input=%b,reset=%b,state=%b, output=%b",in_seq,reset,state_reg,detector_out);
 
     $finish;
 end
 
 endmodule
+
 */
